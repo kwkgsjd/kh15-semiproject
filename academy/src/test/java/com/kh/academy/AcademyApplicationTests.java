5fa50d7301256546781334c28e87fe1a1e7f16c0
kwkgsjd <kwkgsjd0410@gmail.com>
2025-02-19 10:29:23 +0900

    최초 프로젝트 생성 및 초기 설정 구현

---
+++ academy/src/test/java/com/kh/academy/AcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
