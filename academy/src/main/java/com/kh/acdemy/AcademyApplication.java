3cf2e998dd81dbd77cc24bc65372ffd04f47d108
kwkgsjd <kwkgsjd0410@gmail.com>
2025-02-25 17:33:21 +0900

    최초 프로젝트 생성 및 초기 설정 구현

---
+++ academy/src/main/java/com/kh/acdemy/AcademyApplication.java
@@ -0,0 +1,13 @@
+package com.kh.acdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyApplication.class, args);
+	}
+
+}
